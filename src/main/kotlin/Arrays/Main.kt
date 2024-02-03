919871fd67acd009d76f26ef0482a54373b95bba
asirialwis <asirichathurika900@gmail.com>
2024-02-03 23:17:46 +0530

    Tute01 add

---
+++ src/main/kotlin/Arrays/Main.kt
@@ -0,0 +1,6 @@
+package Arrays
+
+fun main(){
+    var carBrands = arrayOf("Toyota","Nissan" , "Mazda" , "Suzuki")
+    println(carBrands.size)
+}
---
+++ src/main/kotlin/function/Main.kt
@@ -0,0 +1,11 @@
+package function
+
+fun printage( a: Int , b:String){
+    println("The age of $b is$a")
+}
+
+
+fun main(){
+    println("Hello Asiri")
+    printage(23,"Asiri")
+}
